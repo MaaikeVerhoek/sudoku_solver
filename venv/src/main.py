c00b9ea9116b907338ceb7015ef7f2816bb6262c
verho534 <maaike.verhoek@kpn.com>
2020-02-20 11:00:10 +0100

    changed variable names

--- venv/src/main.py
+++ venv/src/main.py
@@ -9,23 +9,23 @@ import src.dataprep.setup_sudoku as ss
 
 Vals, Rows, Cols, Boxes = ss.init_sudoku()
 
-prob, choices = mod.init_model(Vals=Vals, Rows=Rows, Cols=Cols)
+prob_init, choices = mod.init_model(Vals=Vals, Rows=Rows, Cols=Cols)
 
-prob = go.objective_function(prob=prob)
+prob_w_obj = go.objective_function(prob=prob_init)
 
-prob = cons.add_general_constraints(Rows=Rows, Cols=Cols, Vals=Vals, Boxes=Boxes, choices=choices, prob=prob)
+prob_cons = cons.add_general_constraints(Rows=Rows, Cols=Cols, Vals=Vals, Boxes=Boxes, choices=choices, prob=prob_w_obj)
 
-prob = cons.add_starting_constraints(prob=prob, choices=choices)
+prob_start = cons.add_starting_constraints(prob=prob_cons, choices=choices)
 
 
 # The problem data is written to an .lp file
-prob.writeLP("Sudoku.lp")
+prob_start.writeLP("Sudoku.lp")
 
 # The problem is solved using PuLP's choice of Solver
-prob.solve()
+prob_start.solve()
 
 # The status of the solution is printed to the screen
-print("Status:", LpStatus[prob.status])
+print("Status:", LpStatus[prob_start.status])
 
 # The solution is written to the sudokuout.txt file
 ps.print_sol(Rows=Rows, Cols=Cols, Vals=Vals, choices=choices)
